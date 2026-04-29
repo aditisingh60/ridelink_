2e04b33de3d13ff1bfadf5c1a2cc4060fd40072c
aditisingh60 <aditisingh6062@gmail.com>
2026-04-30 01:18:00 +0530

    backend ready

--- backend/src/models/User.model.ts
+++ backend/src/models/User.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
-import bcrypt from "bcryptjs";
+import * as bcrypt from "bcryptjs";
 import { UserRole, VehicleType } from "../types";
 
 // Base User Interface
